591da4d67149bb9a1bcbbdde0c2a53ff6bea7bb3
JSSaini07 <mr.jssaini07@gmail.com>
2016-03-30 22:15:22 +0530

    intitial commit

--- editor/javascript/index.js
+++ editor/javascript/index.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function(){
+    $('.title').html(sessionStorage.title);
+    $('.content').html(sessionStorage.content);
+    animateContent();
+    animatePublish();
     $('.title').on('keyup',animateContent);   // to show contentPanel only when title is not empty 
     $('.content').on('keyup',animatePublish); // to show publish button only when content is not empty
 })
@@ -8,6 +12,8 @@ $(document).ready(function(){
 // animateContent runs on every keyPress in title div and if title div is not empty it displays content panel
 
 function animateContent(){
+        console.log(sessionStorage.title);
+        sessionStorage.title=document.getElementsByClassName('title')[0].innerHTML;
         if($('.title').html()=="")
         {
             $('.content').clearQueue().animate({'marginLeft':'120%'},400,function(){$('.content').css({'display':'none'});});
@@ -27,6 +33,8 @@ function animateContent(){
 // animatePublish runs on every keyPress in content div and if content div is not empty it displays publish button
 
 function animatePublish(){
+    console.log(sessionStorage.content);
+        sessionStorage.content=document.getElementsByClassName('content')[0].innerHTML
         if($('.content').html()=="")
         {
             $('#publish').clearQueue().animate({'marginLeft':'120%'},400,function(){$('#publish').css({'display':'none'});});
--- editor/javascript/preview.js
+++ editor/javascript/preview.js
@@ -1,12 +1,7 @@
 
 function preview() {
     $(document).ready(function(){
-        title = $('.title').text();
-        content = $('.content').text();
-        $('#publish').addClass('hide');
-        $('#inputText').slideUp(400,function(){
-            $('#displayText').slideDown(600);
-        });
+        $('#displayText').slideDown(600);
         $('#displayTitle').html(title);
         $('#displayContent').html(content);
     })
