e71a835013884f54e22be4510216858d07757336
JSSaini07 <mr.jssaini07@gmail.com>
2016-12-08 22:43:00 +0530

    setup basic layout

--- TodoApp-React/index.js
+++ TodoApp-React/index.js
@@ -1,8 +1,48 @@
 
-var Component=React.createClass({
+var Event=React.createClass({
 	render:function(){
-		return <h1>Hello World</h1>
+		return <h1>{this.props.val}</h1>
 	}
 });
 
-ReactDOM.render(<Component/>,document.getElementById('root'));
+var EventAdder=React.createClass({
+	render:function(){
+		return (
+			<div>
+				<input type="text" className='addEvent'></input>
+				<div className='addEventButton'>Add</div>
+			</div>
+			);
+	}
+});
+
+var Board=React.createClass({
+	getInitialState:function() {
+		var events=[];
+		return {events:events};
+	},
+	renderEmpty:function(){
+		return <div className="emptyTask"> No Task Added :( </div>
+	},
+	renderNormal:function(){
+		return (
+			<div>
+			{
+				this.state.events.map(function(text,i){
+					return <Event val={text}/>
+				})
+			}
+			</div>
+		);
+	},
+	render:function(){
+		return (
+		<div>
+			<EventAdder/>
+			<div>{(this.state.events.length==0)?this.renderEmpty():this.renderNormal()}</div>;
+		</div>
+		);
+	}
+});
+
+ReactDOM.render(<Board/>,document.getElementById('root'));
