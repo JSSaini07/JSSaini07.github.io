62f34b3e3b3c14f5f61d1c5af7b7db4160c5fe67
JSSaini07 <mr.jssaini07@gmail.com>
2016-12-14 17:26:52 +0530

    update urls

--- localWeatherApplication/index.js
+++ localWeatherApplication/index.js
@@ -6,11 +6,13 @@ var tempFahrenheit='';
 var backgrounds={
 	'Clear':'https://media.giphy.com/media/lyVNcb1n5Ob0Q/giphy.gif',
 	'Clouds':'https://media.giphy.com/media/3yPbhOd3gvJXq/giphy.gif',
-	'Rain':'https://media.giphy.com/media/3o6ozmF4ZGOG3DWZyg/giphy.gif'
+	'Rain':'https://media.giphy.com/media/3o6ozmF4ZGOG3DWZyg/giphy.gif',
+	'Smoke':'https://media.giphy.com/media/3yPbhOd3gvJXq/giphy.gif',
+	'Haze':'https://media.giphy.com/media/hLP8nZ7HmukyA/giphy.gif'
 }
 
 $(document).ready(function(){
-	$.getJSON('http://ipinfo.io', function(data){
+	$.getJSON('https://ipinfo.io', function(data){
   		city=data.city;
   		country=data.country;
   		$('.city').text(city+', '+country);
@@ -33,17 +35,20 @@ $(document).ready(function(){
 });
 
 function getWeather() {
+  console.log('http://api.openweathermap.org/data/2.5/weather?q='+city+','+country+'&units=metric&appid=f1a3d4344c446b4b097a4c121e4ba71d');
 	$.ajax({
 		url:'http://api.openweathermap.org/data/2.5/weather?q='+city+','+country+'&units=metric&appid=f1a3d4344c446b4b097a4c121e4ba71d',
 		method:'GET',
 		success:function(r){
+      console.log(r);
 			tempCelsius=parseInt(r.main.temp);
 			tempFahrenheit=parseInt((tempCelsius*9)/5)+32;
 			$('.weatherContent').text(tempCelsius);
 			$('.degCelsius').addClass('active');
 			$('.weatherDescription').text(r.weather[0].description)
 			$('.weather').css({'display':'inline-block'});
-			$('.weather').css({'background':'url("'+backgrounds[r.weather[0].main]+'")','background-size':'100% 94vh','background-repeat':'no-repeat'});
+      $('.weather').css({'background':'url("'+backgrounds[r.weather[0].main]+'")','background-size':'100% 94vh','background-repeat':'no-repeat'});
 		}
 	})
 }
+
