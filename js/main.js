53f8ecaa60dde89fad9c113cc4dfd7aac6e59491
JSSaini07 <mr.jssaini07@gmail.com>
2018-12-22 20:36:25 +0530

    update portfolio

--- js/main.js
+++ js/main.js
@@ -1,182 +1,123 @@
+var monitorScroll = true;
 
 $(document).ready(function(){
-  layoutInit();
-  applyEvents();
+  handleProjectFilters();
   placeTimelines();
-});
-
-function layoutInit(){
-  var left = $('.selected-menu-item')[0].getClientRects()[0].left;
-  $('.moving-menu-item').css({'display':'inline-block','left':left+'px'});
-}
-
-function applyEvents(){
-
-  var header = $('#header');
-  var menu_items = $('.menu-item');
-  var section_ids = [];
-  var scroll_location = [];
-  menu_items.map(function(i,j){
-      var section_id = j.getAttribute('target');
-      section_ids.push(section_id);
-      scroll_location.push($('#'+section_id)[0].getClientRects()[0].top);
-  });
-
-  $('.menu-item').on('mouseenter',function(){
-    $('.moving-menu-item').clearQueue();
-    var left = this.getClientRects()[0].left;
-    $('.moving-menu-item').animate({'left':left+'px'},200);
-  });
-  $('.menu-item').on('mouseleave',function(){
-    $('.moving-menu-item').clearQueue();
-    var left = $('.selected-menu-item')[0].getClientRects()[0].left;
-    $('.moving-menu-item').animate({'left':left+'px'},200);
-  });
-  $('.menu-item').on('click',function(){
-    $('.selected-menu-item').removeClass('selected-menu-item');
-    $(this).addClass('selected-menu-item');
-    console.log($('#'+$(this).attr('target')).offset().top);
+  $('#intro-section #intro-header #contact').click(function(){
     $('html, body').animate({
-        scrollTop: $('#'+$(this).attr('target')).offset().top+document.body.scrollTop
+      scrollTop: $('#contact-section').offset().top,
     }, 800);
   });
-  $('.filter-item').on('click',function(){
-    if(this.innerText!='All' && !$(this).hasClass('.filter-item-selected')){
-      $($('.filter-item')[0]).removeClass('filter-item-selected');
-    }
-    if($(this).hasClass('filter-item-selected')){
-      $(this).removeClass('filter-item-selected');
-    } else{
-      $(this).addClass('filter-item-selected');
-    }
-    if($('.filter-item-selected').length==0){
-      $('.filter-item')[0].click();
-    }
-    renderProjects();
-  });
+});
 
-  $(document.body).on('scroll',function(){
+function showSkillProgress() {
+  var skills = $('#skill-content-container .skill-progress');
+  for(var i=0;i<skills.length;i++) {
+    var skillItem = $(skills[i]);
+    var progress = skillItem.data('progress');
+    skillItem.css({opacity: '1.0'});
+    skillItem.animate({width: progress+'%'}, 2000);
+  }
+}
 
-    // logic for fixing header on scroll
-    header_offset_top = header.offset().top;
-    if(header_offset_top<=0){
-        header.addClass('header-fixed');
-    }
-    //logic to unfix header
-    if($('#'+section_ids[1]).offset().top>=0){
-      header.removeClass('header-fixed');
-    }
+function scrollEvents() {
+  if($('#skill-container')[0].getBoundingClientRect().y < 0.95 * window.innerHeight) {
+    monitorScroll = false;
+    showSkillProgress();
+  }
+}
 
-    //logic to fill skills on scroll
-    if($('#'+section_ids[1]).offset().top<=100){
-      fillSkills();
-    }
+var monitorScrollInterval = setInterval(function(){
+  if(!monitorScroll){
+    clearInterval(monitorScrollInterval);
+  }
+  scrollEvents();
+}, 200);
 
-    //logic for last section
-    if($(document.body).scrollTop() + $(document.body).height() == $(document).height()){
-        $('.selected-menu-item').removeClass('selected-menu-item');
-        $(menu_items[menu_items.length-1]).addClass('selected-menu-item');
-        $(menu_items[menu_items.length-1]).trigger('mouseleave');
-        return;
-    }
-
-    var scroll_loc = $(document.body).scrollTop();
-    for(var i=0;i<scroll_location.length-1;i++){
-      if(scroll_loc+100>scroll_location[i] && scroll_loc+100<scroll_location[i+1]){
-          $('.selected-menu-item').removeClass('selected-menu-item');
-          $(document.querySelectorAll('.menu-item[target='+section_ids[i]+']')[0]).addClass('selected-menu-item');
-          $(document.querySelectorAll('.menu-item[target='+section_ids[i]+']')[0]).trigger('mouseleave');
-          return;
+function handleProjectFilters() {
+  $('.project-technology').on('click',function(){
+    var currentSelectedFilters = $('.filter-selected');
+    if($(this).hasClass('filter-selected')){
+      $(this).removeClass('filter-selected');
+      if($('.filter-selected').length === 0){
+        $('.project-technology[data-filter="All"]').addClass('filter-selected');
       }
     }
-
-
+    else if((currentSelectedFilters.length === 1 && $(currentSelectedFilters[0]).data('filter') === 'All') || $(this).data('filter') === 'All') {
+      currentSelectedFilters.removeClass('filter-selected');
+      $(this).addClass('filter-selected');
+    }
+    else {
+      $(this).addClass('filter-selected');
+    }
+    var selectedTags = $('.filter-selected').map((index,elem)=>$(elem).data('filter'));
+    selectedTags = Array.from(selectedTags);
+    handleProjectVisibility(selectedTags);
   });
-
-  $('#timeline-section svg circle').on('mouseenter',function(){
-      var targetWorkDetails = $(this).attr('target');
-      $('.'+targetWorkDetails).clearQueue();
-      $('.'+targetWorkDetails).slideDown(200);
-  });
-
-  $('#timeline-section svg circle').on('mouseleave',function(){
-      var targetWorkDetails = $(this).attr('target');
-      $('.'+targetWorkDetails).clearQueue();
-      $('.'+targetWorkDetails).slideUp(200);
-  });
-
-  $('#intro-button').on('click',function(){
-    $('.menu-item')[1].click();
-  })
-
 }
 
-function fillSkills(){
-  var skills = $('.right-content .skill-container .skill-value-fill');
-  var width = $('.right-content .skill-container .skill-value')[0].getClientRects()[0].width;
-  for(var i=0;i<skills.length;i++){
-    var skill = skills[i];
-    var parentContainer = $(skill).parent();
-    var value = parseInt(parentContainer.attr('value'));
-    var calculatedWidth = (value*width)/100;
-    parentContainer.find('.skill-value-fill-name').text(value+'%');
-    $(skill).animate({'width':calculatedWidth+'px'},1400);
-  }
-}
-
-function renderProjects(){
-  $('.projectItem').addClass('hiddenProjectItem');
-  var filteredItems = document.querySelectorAll('.filter-item-selected');
-  var filteredItemsMap = {};
-  filteredItems = $(filteredItems).map(function(e,r){return r.innerText;})
-  for(var i=0;i<filteredItems.length;i++){
-    filteredItemsMap[filteredItems[i].split(' ').join('')]=1;
-  }
-  if(filteredItemsMap['All']==1){
-    $('.hiddenProjectItem').removeClass('hiddenProjectItem');
+function handleProjectVisibility(selectedTags) {
+  var projectItems = $('.project-item');
+  if(selectedTags.length === 1 && selectedTags[0] === 'All') {
+    projectItems.removeClass('project-hidden');
     return;
-  }else {
-    var projectItems = $('.projectItem');
-    for(var i=0;i<projectItems.length;i++){
-      var currentProjectItem = $(projectItems[i]);
-      var projectTags = currentProjectItem.find('.tag')
-      projectTags = projectTags.map(function(i,j){return j.innerText;})
-      var showFlag = false;
-      for(j=0;j<projectTags.length;j++){
-        if(filteredItemsMap[projectTags[j]]==1){showFlag=true;break;}
-      }
-      if(showFlag){
-        currentProjectItem.removeClass('hiddenProjectItem');
-      }
-    }
   }
+  projectItems.map((index, element) => {
+    var tags = $(element).find('.tag');
+    tags = tags.filter((index,tag) => {
+      return selectedTags.indexOf($(tag).text())>=0;
+    });
+    $(element).removeClass('project-hidden');
+    if(tags.length === 0){
+      $(element).addClass('project-hidden');
+    }
+  });
 }
 
 function placeTimelines(){
-  var svg = $('#timelinePath svg');
-  var svg_coords = svg[0].getClientRects()[0];
+  var svg = $('#timeline-path svg');
+  var svg_coords = svg[0].getBoundingClientRect();
   var tooltip1 = $('.customTooltipFirst');
   var tooltip2 = $('.customTooltipSecond');
   var tooltip3 = $('.customTooltipThird');
   var tooltip4 = $('.customTooltipFourth');
+  var tooltip5 = $('.customTooltipFifth');
 
   var x1 = svg_coords.x+20;
   var x2 = svg_coords.x+60;
   var x3 = svg_coords.x+20;
   var x4 = svg_coords.x+60;
+  var x5 = svg_coords.x+20;
 
-  var y1 = svg.find('circle')[1].getClientRects()[0].y+10 - (tooltip1[0].getClientRects()[0].height/2) - $('#timeline-section')[0].getClientRects()[0].y;
-  var y2 = svg.find('circle')[2].getClientRects()[0].y+10 - (tooltip2[0].getClientRects()[0].height/2) - $('#timeline-section')[0].getClientRects()[0].y;
-  var y3 = svg.find('circle')[3].getClientRects()[0].y+10 - (tooltip3[0].getClientRects()[0].height/2) - $('#timeline-section')[0].getClientRects()[0].y;
-  var y4 = svg.find('circle')[4].getClientRects()[0].y+10 - (tooltip4[0].getClientRects()[0].height/2) - $('#timeline-section')[0].getClientRects()[0].y;
+  var y1 = svg.find('circle')[1].getBoundingClientRect().y+10 - (tooltip1[0].getBoundingClientRect().height/2) - $('#timeline-section')[0].getBoundingClientRect().y;
+  var y2 = svg.find('circle')[2].getBoundingClientRect().y+10 - (tooltip2[0].getBoundingClientRect().height/2) - $('#timeline-section')[0].getBoundingClientRect().y;
+  var y3 = svg.find('circle')[3].getBoundingClientRect().y+10 - (tooltip3[0].getBoundingClientRect().height/2) - $('#timeline-section')[0].getBoundingClientRect().y;
+  var y4 = svg.find('circle')[4].getBoundingClientRect().y+10 - (tooltip4[0].getBoundingClientRect().height/2) - $('#timeline-section')[0].getBoundingClientRect().y;
+  var y5 = svg.find('circle')[5].getBoundingClientRect().y+10 - (tooltip5[0].getBoundingClientRect().height/2) - $('#timeline-section')[0].getBoundingClientRect().y;
 
   tooltip1.css({'left':x1+'px', 'top':y1+'px'});
   tooltip2.css({'right':x2+'px', 'top':y2+'px'});
   tooltip3.css({'left':x3+'px', 'top':y3+'px'});
   tooltip4.css({'right':x4+'px', 'top':y4+'px'});
-
-  $('.workDetailsFirst').css({'right':(x1+29)+'px','top':y1+'px'});
-  $('.workDetailsSecond').css({'left':(x2-11)+'px','top':y2+'px'});
-  $('.workDetailsThird').css({'right':(x3+29)+'px','top':y3+'px'});
-  $('.workDetailsFourth').css({'left':(x4-11)+'px','top':y4+'px'});
+  tooltip5.css({'left':x5+'px', 'top':y5+'px'});
 }
+
+function handleParallax() {
+  var containers = $('.parallax');
+  for(var i=0;i<containers.length;i++) {
+    var currentContainer = $(containers[i]);
+    $(currentContainer).css({position: 'relative'});
+    var top = currentContainer.offset().top;
+    var height = currentContainer.height();
+    if((top < window.innerHeight) && (top + height >= 0)) {
+      var change = (top - window.innerHeight)/20;
+      currentContainer.css({
+        top: change,
+      })
+    }
+  }
+}
+
+window.onresize = placeTimelines;
+
+// setInterval(handleParallax, 1);
