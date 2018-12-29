6e143264235b96e2482810607d7d58c81ce7b06f
JSSaini07 <mr.jssaini07@gmail.com>
2018-12-29 20:49:16 +0530

    update Gruntfile to inline all assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     require('jit-grunt')(grunt);
+    const mozjpeg = require('imagemin-mozjpeg');
     grunt.initConfig({
         less: {
             development: {
@@ -26,17 +27,32 @@ module.exports = function(grunt) {
               dest: 'index.html'
             }
         },
+        imagemin: {            
+            dynamic: {
+                options: {
+                    optimizationLevel: 7,
+                    svgoPlugins: [{removeViewBox: false}],
+                    use: [mozjpeg()] 
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/images'
+                }]
+            }
+        },
         watch: {
             styles: {
                 files: ["src/css/*.less", "src/css/fonts.css"],
-                tasks: ["less"],
+                tasks: ["less", "inline"],
                 options: {
                     nospawn: true
                 }
             },
             js: {
                 files: ["src/js/jquery-3.3.1.min.js", "src/js/fontawesome-v5.0.13.min.js", "src/js/main.js"],
-                tasks: ["uglify"],
+                tasks: ["uglify", "inline"],
                 options: {
                     nospawn: true
                 }
@@ -50,5 +66,5 @@ module.exports = function(grunt) {
             }
         }
     });
-    grunt.registerTask("default", ["less", "uglify", "inline", "watch"]);
+    grunt.registerTask("default", ["less", "uglify","imagemin", "inline", "watch"]);
 };
